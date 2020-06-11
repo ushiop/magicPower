53ba314b3c3a4d6ddf0e3c0cc7d50e60bcf229aa
ushiop <397624588@qq.com>
2020-06-11 23:06:53 +0800

    现在将在游戏内直接显示变动属性

--- Commands/testCommand.cs
+++ Commands/testCommand.cs
@@ -9,12 +9,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
-
+using Newtonsoft.Json.Linq; 
+ 
 
 namespace magicPower.Commands
 {
 	public class testCommand : ModCommand
 	{
+		private JObject  playerInfo=new JObject();
+
 		public override CommandType Type
 			=> CommandType.Chat;
 
@@ -27,10 +30,32 @@ namespace magicPower.Commands
 			 
 			var type = caller.Player.GetType();
 			var Fields = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
-            mod.Logger.Info("===========");
+            string name;
+			mod.Logger.Info("==========="); 
             foreach (var info in Fields)
 				{
-					mod.Logger.Info(info.Name.ToString()+"/"+info.GetValue(caller.Player).ToString()); // at log-level INFO
+					//mod.Logger.Info(info.Name.ToString()+"/"+info.GetValue(caller.Player).ToString()); // at log-level INFO
+					name=info.Name.ToString();
+					if(name=="gemCount"||
+						name=="miscCounter"||
+						name=="infernoCounter"||
+						name=="shadowCount"||
+						name=="lifeRegenCount"||
+						name=="manaRegenCount"||
+						name=="lifeRegen"||
+						name=="handon"||
+						name=="shoe"){
+							continue;
+						}
+						
+					if(playerInfo[name]==null){
+						playerInfo[name]=info.GetValue(caller.Player).ToString();
+					}else{
+						if(info.GetValue(caller.Player).ToString()!=playerInfo[name].ToString()){
+							Main.NewText("属性["+name+"]变化了,旧/新值["+playerInfo[name]+"/"+info.GetValue(caller.Player).ToString()+"]");
+							playerInfo[name]=info.GetValue(caller.Player).ToString();
+						}
+					}
 				}
             Main.NewText("player类的属性已输出完毕。"); 
 		
